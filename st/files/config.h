2a3c99c874996d7159e2507586e36b065c4ee465
Ben Hamlin <protob3n@gmail.com>
2015-08-20 22:31:18 -0700

    Modify st keyboard shortcuts

--- st/files/config.h
+++ st/files/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "profont:pixelsize=14:antialias=false:autohint=false";
+static char font[] = "Profont:size=14";
 static int borderpx = 1;
 
 /*
@@ -114,6 +114,11 @@ static unsigned int defaultcs = 7;
 static unsigned int defaultitalic = 5;
 static unsigned int defaultunderline = 7;
 
+void smcup() {
+}
+void rmcup() {
+}
+
 /* Internal mouse shortcuts. */
 /* Beware that overloading Button1 will disable the selection. */
 static Mousekey mshortcuts[] = {
@@ -129,12 +134,13 @@ static Shortcut shortcuts[] = {
   /* mask                 keysym          function        argument */
 	{ ControlMask,           XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,             XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,            XK_Print,       printsel,       {.i =  0} },
-	{ ControlMask,           XK_equal,       xzoom,          {.i = +1} },
+        { XK_ANY_MOD,            XK_Print,       printsel,       {.i =  0} },
+        { ControlMask,           XK_equal,       xzoom,          {.i = +1} },
 	{ ControlMask,           XK_minus,       xzoom,          {.i = -1} },
 	{ ControlMask,           XK_0,           xzoomreset,     {.i =  0} },
-	{ ControlMask|ShiftMask, XK_V,           selpaste,       {.i =  0} },
-	{ ControlMask|ShiftMask, XK_P,           clippaste,      {.i =  0} },
+	{ ControlMask|ShiftMask, XK_P,           selpaste,       {.i =  0} },
+	{ ControlMask|ShiftMask, XK_V,           clippaste,      {.i =  0} },
+	{ ControlMask|ShiftMask, XK_C,           clipcopy,       {.i =  0} },
 	{ MODKEY,                XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
